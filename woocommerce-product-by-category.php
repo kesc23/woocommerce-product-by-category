4cee95f38c9f0f45a5f9341e0b75e1c107d6e270
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-05-26 20:56:02 -0300

    Update woocommerce-product-by-category.php

--- woocommerce-product-by-category.php
+++ woocommerce-product-by-category.php
@@ -5,7 +5,7 @@
  * Description: Tenha em seu site um ótimo componente responsivo para mostrar os produtos e suas categorias na sua loja
  * Author URI: https://felizex.press
  * Copyright: (c) 2021 Kesc23
- * Version: 0.2.0
+ * Version: 0.6.0
  * Licence: GPL v3.0 or Later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
 **/
