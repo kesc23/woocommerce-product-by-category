57376628751905fb0c3c6a9afeab62f78934691e
Kevin Campos <kevincampos.2475@aluno.una.br>
2021-05-28 00:20:22 -0300

    Dependency Added
    Added a conditional to plugin: it will not start if woocommerce is deactivated

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -8,26 +8,20 @@ if ( ! defined( 'ABSPATH' )){
 // Function to 
 function wpc_show_messages( string $wpc_message, string $wpc_message_type)
 {
-    //error message variable
+    //error & class message variables
     $wpc_error = null;
+    $wpc_message_class = null;
 
-    if ($wpc_message_type == error)
+    if ($wpc_message_type == "error")
     {
         $wpc_message_class = '"error"';
-        $wpc_error = "<strong>ERROR</strong>";
+        $wpc_error = "<strong>ERROR</strong>: ";
     } elseif ($wpc_message_type == update)
     {
         $wpc_message_class = '"error"';
     }
     
-    ob_start();?>
-    <div class=<?php echo $wpc_message_class ?>>
-        <p><?php echo $wpc_error . $wpc_message ?></p>
-    </div>
-    <?php
-        echo ob_get_clean();
-    
-    //deactivate_plugins( 'Woocommerce Product By Category', bool $silent = false, bool|null $network_wide = null )
+    echo ('<div class=' . $wpc_message_class . '><p>' . $wpc_error . $wpc_message . '</p></div>');
 }
 
 add_action( 'admin_messages', 'wpc_show_messages', 10, 2);
--- woocommerce-product-by-category.php
+++ woocommerce-product-by-category.php
@@ -5,7 +5,7 @@
  * Description: Tenha em seu site um ótimo componente responsivo para mostrar os produtos e suas categorias na sua loja
  * Author URI: https://felizex.press
  * Copyright: (c) 2021 Kesc23
- * Version: 0.5.0
+ * Version: 0.6.0
  * Licence: GPL v3.0 or Later
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
 **/
@@ -24,3 +24,18 @@ if ( ! defined('__WPCDIR__'))
 require_once (__WPCDIR__) . '/includes/wpc-functions.php';
 
 require_once (__WPCDIR__) . '/includes/wpc-shortcodes.php';
+
+require_once (__WPCDIR__) . '/admin/admin-functions.php';
+
+/**
+ * ACTION HOOK TO VERIFY IF WOOCOMMERCE IS ACTIVE
+ * CASE FALSE, DISPLAY ERROR MESSAGE AND DEACTIVATE PLUGIN
+ * 'wpc_show_messages' function args: ( string $wpc_message, string $wpc_message_type)
+**/
+
+if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) )
+{
+    do_action( 'admin_messages' , 'WooCommerce is not Activated. Please Activate Woocommerce', 'error');
+
+    deactivate_plugins( 'woocommerce-product-by-category/woocommerce-product-by-category.php' );
+}
